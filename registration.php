1f58e3ddfd3221189a1d408b37de9bd368eb4076
Sam Turner <sam@digi.ltd.uk>
2019-07-18 01:59:29 +0100

    first

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Attachatag_AutoUpdateCartItemQty',
+    __DIR__
+);
